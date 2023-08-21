fc476f8884fc4a726d2b25b8e42cbceb605561fc
MisterPoney <marcosandreo2003@gmail.com>
2023-08-20 22:19:16 -0300

    [form] - > 95% done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/template/header/header.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { FormComponent } from './components/page/form/form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/page/form/form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { retry } from 'rxjs';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+
+export class FormComponent {
+}
